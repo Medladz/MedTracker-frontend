cbaa796dffd6036b0364df910b294f78cc3c4636
FinnLiersch <54947828+FinnLiersch@users.noreply.github.com>
2020-06-19 10:44:29 +0200

    Substannce api call not null fix

--- app/src/main/java/com/example/medtracker/NewSubstance.kt
+++ app/src/main/java/com/example/medtracker/NewSubstance.kt
@@ -2,8 +2,11 @@ package com.example.medtracker
 
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,23 +35,46 @@ class NewSubstance : FragmentActivity() {
         fetchJson(apiToken)
     }
     fun fetchJson(t : String?){
-        val apiUrl = "http://192.168.43.193:8080/creators/$t/drugs?withVerified=false"
+        val apiUrl = "http://192.168.2.19:8080/creators/1/drugs"
         val request = Request.Builder().url(apiUrl).build()
         val client = OkHttpClient()
         client.newCall(request).enqueue(object: Callback{
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
                 val gson = GsonBuilder().create()
+
                 val drugFeed = gson.fromJson(body, DrugFeed::class.java)
-                runOnUiThread {
-                    Substance_recyclerview.adapter = SubstanceAdapter(drugFeed)
+
+                if (drugFeed != null) {
+                    runOnUiThread {
+                        Substance_recyclerview.adapter = SubstanceAdapter(drugFeed)
+                    }
+                } else {
+                    runOnUiThread(Runnable() {
+                        val error = findViewById<TextView>(R.id.ErrorTextView)
+                        error.text = "Error! Could not load data"
+                        error.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25f)
+                        error.setTextColor(Color.WHITE)
+
+                    })
                 }
             }
+
             override fun onFailure(call: Call, e: IOException) {
                 println("failed to execute")
+                runOnUiThread(Runnable() {
+                    val error = findViewById<TextView>(R.id.ErrorTextView)
+                    error.text = "Error! Could not load data"
+                    error.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25f)
+                    error.setTextColor(Color.WHITE)
+
+                })
+
             }
         })
     }
+}
+
 }
 
 
